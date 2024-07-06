fff948948544b752d1141b848d845f49c5814386
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-07-06 18:29:23 +0530

    Update dashboard-header.js

--- src/components/dashboard-header.js
+++ src/components/dashboard-header.js
@@ -1,8 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { fetchBatchData } from "./data";
+import { fetchBatchData, fetchLoginData } from "./data";
 import axios from "axios";
 
-const DashboardHeader = ({selectBatch, studentData, todayDate, Alert}) => {
+export const date_time = () => {
+    const today = new Date();
+    const day = today.getDate();
+    const options = { month: 'long' };
+    const month = today.toLocaleDateString('en-US', options);
+    const year = today.getFullYear();
+    const hour = today.getHours();
+    const min = today.getMinutes();
+    const sec = today.getSeconds();
+
+    return `${day} ${month} ${year} ${hour} ${min} ${sec}`;
+};
+
+const DashboardHeader = ({selectBatch, studentData, todayDate, Alert, selected_Cls, class_Data, user, login_Data, update_Data}) => {
     const [batchData,setBatchData] = useState([]);
 
     const getBatches = async () => {
@@ -14,10 +27,6 @@ const DashboardHeader = ({selectBatch, studentData, todayDate, Alert}) => {
         getBatches();
     }, []);
 
-    useEffect(() => {
-        getBatches();
-    }, [batchData]);
-
     let batch_Data = true;
     if (!Array.isArray(batchData)){
         batch_Data = false;
@@ -26,7 +35,7 @@ const DashboardHeader = ({selectBatch, studentData, todayDate, Alert}) => {
     let stdLength = 0;
     if (studentData){
         studentData.forEach(data=>{
-            if (selectBatch === 'All' || data.BatchName === selectBatch) {
+            if ((selectBatch === 'All' || data.BatchName === selectBatch) && (data.Class === selected_Cls || selected_Cls === "All")){
                 stdLength++;
             };
         });
@@ -38,7 +47,7 @@ const DashboardHeader = ({selectBatch, studentData, todayDate, Alert}) => {
     let interviews = 0;
     if (batch_Data){
         batchData.forEach((batch,index)=> {
-            if (selectBatch === 'All' || batch.BatchName === selectBatch) {
+            if ((selectBatch === 'All' || batch.BatchName === selectBatch) && (batch.Class === selected_Cls || selected_Cls === 'All')) {
                 classes += parseInt(batch.Classes.split(' ')[0]);
                 mockTests += parseInt(batch.MockTests.split(' ')[0]);
                 caseStudies += parseInt(batch.CaseStudies.split(' ')[0]);
@@ -48,6 +57,7 @@ const DashboardHeader = ({selectBatch, studentData, todayDate, Alert}) => {
     };
 
     const setProgress = (percent,idx) => {
+        const percentage = (percent === 0) ? 1 : percent;
         let progressBar = "";
         let progressText = "";
         progressBar = document.querySelector(`.progress-bar-${idx}`);
@@ -55,7 +65,7 @@ const DashboardHeader = ({selectBatch, studentData, todayDate, Alert}) => {
             if(progressBar){
                 const radius = progressBar.r.baseVal.value;
                 const circumference = 2 * Math.PI * radius;
-                const offset = circumference - (percent / 100) * circumference;
+                const offset = circumference - (percentage / 100) * circumference;
                 progressBar.style.strokeDashoffset = offset;
                 progressText.textContent = percent < 10 ? `0${percent}%` : `${percent}%`;
             };
@@ -67,40 +77,125 @@ const DashboardHeader = ({selectBatch, studentData, todayDate, Alert}) => {
         let stdStudyCnt = 0;
         let stdInterviewCnt = 0;
             studentData.forEach((data,index)=>{
-          if (selectBatch === 'All' || data.BatchName === selectBatch){
+          if ((selectBatch === 'All' || data.BatchName === selectBatch) && (data.Class === selected_Cls || selected_Cls === "All")){
             stdClassCnt += parseInt(data.Classes.split(' ')[0]);
             stdMockCnt += parseInt(data.MockTests.split(' ')[0]);
             stdStudyCnt += parseInt(data.CaseStudies.split(' ')[0]);
             stdInterviewCnt += parseInt(data.Interviews.split(' ')[0]);
-          }  
+          }
         });
+
         let class_Score = (stdClassCnt / (classes * stdLength)) * 100;
-        if (!class_Score){
+        if (class_Score === Infinity || class_Score <= 0 || !class_Score){
             class_Score = 0;
         }
         setProgress(Math.floor(class_Score),2);
+
         let mock_Score = (stdMockCnt / (mockTests * stdLength)) * 100;
-        if (!mock_Score){
+        if (mock_Score === Infinity || mock_Score <= 0 || !mock_Score){
             mock_Score = 0;
         }
         setProgress(Math.floor(mock_Score),3);
+        
         let study_Score = (stdStudyCnt / (caseStudies * stdLength)) * 100;
-        if (!study_Score){
+        if (study_Score === Infinity || study_Score <= 0 || !study_Score){
             study_Score = 0;
         }
         setProgress(Math.floor(study_Score),4);
+
         let interview_Score = (stdInterviewCnt / (interviews * stdLength)) * 100;
-        if (!interview_Score){
+        if (interview_Score === Infinity || interview_Score <= 0 || !interview_Score){
             interview_Score = 0;
         }
         setProgress(Math.floor(interview_Score),5);
-        let overAllScore = (class_Score * 0.25) + (mock_Score * 0.25) + (study_Score * 0.25) + (interview_Score * 0.25)
-        if (!overAllScore){
+
+        let overAllScore = ((class_Score * 0.25) || 0) + ((mock_Score * 0.25) || 0) + ((study_Score * 0.25) || 0 ) + ((interview_Score * 0.25) || 0)
+        if (overAllScore === Infinity || overAllScore <= 0 || !overAllScore){
             overAllScore = 0;
         }
         setProgress(Math.floor(overAllScore),1);
     };
     updatePeformance();
+
+    const setClassesProgress = (percent,idx) => {
+        const percentage = (percent === 0) ? 1 : percent;
+        let progressBar;
+        let progressText;
+        progressBar = document.querySelector(`.class-progress-bar-${idx}`);
+        progressText = document.querySelector(`.class-progress-bar-txt-${idx}`);
+            if(progressBar){
+                const radius = progressBar.r.baseVal.value;
+                const circumference = 2 * Math.PI * radius;
+                const offset = circumference - (percentage / 100) * circumference;
+                progressBar.style.strokeDashoffset = offset;
+                progressText.textContent = percent < 10 ? `0${percent}%` : `${percent}%`;
+                // if (percent <= 35){
+                //     progressBar.style.stroke = 'red';
+                // }else if (percent > 35 && percent <= 50){
+                //     progressBar.style.stroke = 'gold';
+                // }else if (percent > 50 && percent <= 75){
+                //     progressBar.style.stroke = 'orange';
+                // }else if (percent > 75){
+                //     progressBar.style.stroke = 'green';
+                // }
+            };
+    };
+
+    const updateClassPerformance = (cls) => {
+        const batchDataArray = [];
+        let totalCalc = 0;
+        let classCnt = 0;
+        let mockCnt = 0;
+        let studyCnt = 0;
+        let interviewCnt = 0;
+        let stdClassCnt = 0;
+        let stdMockCnt = 0;
+        let stdStudyCnt = 0;
+        let stdInterviewCnt = 0;
+        let stdCnt = 0;
+        let batchCnt = 0;
+        batchData.forEach((bthData)=>{
+            if (cls === bthData.Class){
+                classCnt += parseInt(bthData.Classes.split(' ')[0]);
+                mockCnt += parseInt(bthData.MockTests.split(' ')[0]);
+                studyCnt += parseInt(bthData.CaseStudies.split(' ')[0]);
+                interviewCnt += parseInt(bthData.Interviews.split(' ')[0]);
+                studentData.forEach(data=>{
+                    if (cls === data.Class && bthData.BatchName === data.BatchName){
+                        stdCnt++
+                        stdClassCnt += parseInt(data.Classes.split(' ')[0]);
+                        stdMockCnt += parseInt(data.MockTests.split(' ')[0]);
+                        stdStudyCnt += parseInt(data.CaseStudies.split(' ')[0]);
+                        stdInterviewCnt += parseInt(data.Interviews.split(' ')[0]);
+                    }
+                });
+
+                const stdTotal = stdClassCnt + stdStudyCnt + stdMockCnt + stdInterviewCnt;
+                const batchTotal = classCnt + mockCnt + studyCnt + interviewCnt;
+                const denominator = batchTotal * stdCnt;
+                const calculation = denominator !== 0 ? (stdTotal / denominator) * 100 : 0;
+                batchDataArray.push({[bthData.BatchName] : Math.floor(calculation)});
+                classCnt = 0;
+                mockCnt = 0;
+                studyCnt = 0;
+                interviewCnt = 0;
+                stdClassCnt = 0;
+                stdMockCnt = 0;
+                stdStudyCnt = 0;
+                stdInterviewCnt = 0;
+                stdCnt = 0;
+            };
+        });
+
+    batchDataArray.forEach(data=>{
+        totalCalc += parseInt(Object.values(data));
+    });
+
+    const denominator_ = batchDataArray.length * 100;
+    const finalCalc = denominator_ !== 0 ? (totalCalc / denominator_) * 100 : 0;
+
+    return Math.floor(finalCalc);
+    };
 
     const stdAttPopUp = (name,status) => {
         if((!batchData) && (!studentData)){
@@ -121,19 +216,21 @@ const DashboardHeader = ({selectBatch, studentData, todayDate, Alert}) => {
                 batchData.forEach((data,index)=>{
                     const optsImgEle = document.querySelector(`.batch-att-img-${index}`);
                     const appOptImgEle = document.querySelector('.batch-opt-last-img');
-                    if (name === 'Class'){
-                        data.Classes.includes(todayDate) ? optsImgEle.setAttribute('src','images/batch-P.png') : optsImgEle.setAttribute('src','images/batch-A.png');
-                        batchData.every(data=>data.Classes.includes(todayDate)) ? appOptImgEle.setAttribute('src','images/batch-P.png') : appOptImgEle.setAttribute('src','images/batch-A.png'); 
-                    }else if (name === 'MockTest'){
-                        data.MockTests.includes(todayDate) ? optsImgEle.setAttribute('src','images/batch-P.png') : optsImgEle.setAttribute('src','images/batch-A.png');
-                        batchData.every(data=>data.MockTests.includes(todayDate)) ? appOptImgEle.setAttribute('src','images/batch-P.png') : appOptImgEle.setAttribute('src','images/batch-A.png'); 
-                    }else if (name === 'CaseStudy'){
-                        data.CaseStudies.includes(todayDate) ? optsImgEle.setAttribute('src','images/batch-P.png') : optsImgEle.setAttribute('src','images/batch-A.png');
-                        batchData.every(data=>data.CaseStudies.includes(todayDate)) ? appOptImgEle.setAttribute('src','images/batch-P.png') : appOptImgEle.setAttribute('src','images/batch-A.png'); 
-                    }else if (name === 'Interview'){
-                        data.Interviews.includes(todayDate) ? optsImgEle.setAttribute('src','images/batch-P.png') : optsImgEle.setAttribute('src','images/batch-A.png');
-                        batchData.every(data=>data.Interviews.includes(todayDate)) ? appOptImgEle.setAttribute('src','images/batch-P.png') : appOptImgEle.setAttribute('src','images/batch-A.png'); 
-                    }
+                    if (optsImgEle){
+                        if (name === 'Class'){
+                            data.Classes.includes(todayDate) ? optsImgEle.setAttribute('src','images/batch-P.png') : optsImgEle.setAttribute('src','images/batch-A.png');
+                            batchData.every(data=>data.Classes.includes(todayDate)) ? appOptImgEle.setAttribute('src','images/batch-P.png') : appOptImgEle.setAttribute('src','images/batch-A.png'); 
+                        }else if (name === 'MockTest'){
+                            data.MockTests.includes(todayDate) ? optsImgEle.setAttribute('src','images/batch-P.png') : optsImgEle.setAttribute('src','images/batch-A.png');
+                            batchData.every(data=>data.MockTests.includes(todayDate)) ? appOptImgEle.setAttribute('src','images/batch-P.png') : appOptImgEle.setAttribute('src','images/batch-A.png'); 
+                        }else if (name === 'CaseStudy'){
+                            data.CaseStudies.includes(todayDate) ? optsImgEle.setAttribute('src','images/batch-P.png') : optsImgEle.setAttribute('src','images/batch-A.png');
+                            batchData.every(data=>data.CaseStudies.includes(todayDate)) ? appOptImgEle.setAttribute('src','images/batch-P.png') : appOptImgEle.setAttribute('src','images/batch-A.png'); 
+                        }else if (name === 'Interview'){
+                            data.Interviews.includes(todayDate) ? optsImgEle.setAttribute('src','images/batch-P.png') : optsImgEle.setAttribute('src','images/batch-A.png');
+                            batchData.every(data=>data.Interviews.includes(todayDate)) ? appOptImgEle.setAttribute('src','images/batch-P.png') : appOptImgEle.setAttribute('src','images/batch-A.png'); 
+                        }
+                    };
                 });
             }else if (status === 'close'){
                 mainDivEle.style.opacity = '0';
@@ -204,7 +301,7 @@ const DashboardHeader = ({selectBatch, studentData, todayDate, Alert}) => {
                                             data.CaseStudies = `${parseInt(data.CaseStudies.split(' ')[0]) + 1} ${data.id} ${todayDate}`;
                                         }else if (attName === 'Interview'){
                                             data.Interviews = `${parseInt(data.Interviews.split(' ')[0]) + 1} ${data.id} ${todayDate}`;
-                                        }
+                                        };
                                         updateBatchAtt(data,attName,selectedBatch);
                                     };
                                 });
@@ -233,15 +330,7 @@ const DashboardHeader = ({selectBatch, studentData, todayDate, Alert}) => {
         }
     };
 
-    const updateBatchAtt = async (batch_Data,name,batch) => {
-        const data = {
-            id : batch_Data.id,
-            BatchName : batch_Data.BatchName,
-            Classes : batch_Data.Classes,
-            CaseStudies : batch_Data.CaseStudies,
-            MockTests : batch_Data.MockTests,
-            Interviews : batch_Data.Interviews,
-         };
+    const updateBatchAtt = async (data,name,batch) => {
         try {
             let res = await axios.put('http://127.0.0.1:8000/batches/', JSON.stringify(data), {
                 headers: {
@@ -250,6 +339,8 @@ const DashboardHeader = ({selectBatch, studentData, todayDate, Alert}) => {
             });
             if (res.status === 200 || res.status === 201) {
                 Alert('success',`${batch} ${name} Batch Attendance Updated successfully!`);
+                getBatches();
+                update_Data();
             }
         } catch (error) {
             Alert('error','Unfortunately, the batch Attendance addition was unsuccessful.<br/>Please check and try again!',8000);
@@ -272,13 +363,24 @@ const DashboardHeader = ({selectBatch, studentData, todayDate, Alert}) => {
             selectedOpt.setAttribute('data-value',batchName);
         }
         attBatchDivVisibility();
-    }
+    };
 
+    const classPerformanceDiv = (status='close') => {
+        if (user !== "Super Admin")return
+        const divEle = document.querySelector('.all-class-performance-container');
+        divEle.style.opacity = (status === 'open') ? '1' : '0';
+        divEle.style.visibility = (status === 'open') ? 'visible' : 'hidden';
+        setTimeout(()=>{
+            class_Data.forEach((data,index)=>{
+                (status === 'open') ? setClassesProgress(updateClassPerformance(data.Class),index) : setClassesProgress(0,index);
+            });
+        },((status === 'open')) ? 250 : 500);
+    };
 
   return (
     <div>
         <div className="dashboard-header">
-            <div className="header-students-div header-div">
+            <div className="header-students-div header-div" onDoubleClick={()=>(user === "Super Admin") && classPerformanceDiv('open')}>
                 <div className="progress-circle progress-circle-3">
                     <svg width="100" height="100" viewBox="0 0 100 100">
                         <circle className="progress-bg" cx="50" cy="50" r="45"></circle>
@@ -374,11 +476,13 @@ const DashboardHeader = ({selectBatch, studentData, todayDate, Alert}) => {
                     <p className="selected-opt" data-value="">Select Batch</p>
                     <img id="down-arrow" src="images/down-arrow.png" alt=""/>
                 </div>
-                <div className="opts-div">
+                <div className="opts-div" style={{top : '50px'}}>
                 {Array.isArray(batchData) && batchData.map((data,index)=>{
+                    if (selected_Cls === 'All' || data.Class === selected_Cls){
                         return(
                             <li className={`batch-opt-${index} batch-opt`} onClick={()=>attBatchOptSelect(data.BatchName)}><span>{data.BatchName}</span><img className={`batch-att-img-${index}`} src="" width="40px" alt=""/></li>
                         )
+                    }
                     })}
                     <li className="batch-opt-last batch-opt" onClick={()=>attBatchOptSelect('All Batches')}><span>All Batches</span><img className="batch-opt-last-img" src="" width="40px" alt=""/></li>
                 </div>
@@ -387,6 +491,25 @@ const DashboardHeader = ({selectBatch, studentData, todayDate, Alert}) => {
             <span id="X-times-span" onClick={()=>stdAttPopUp(null,'close')}>&times;</span>
         </div>
     </div>
+        <div className="all-class-performance-container">
+            <img src="images/V-CUBE-Logo.png" className="main-logo"/>
+            <span className="close-container" onClick={classPerformanceDiv}>&times;</span>
+            <h1 style={{marginTop : '120px'}}>Class Performance</h1>
+            <div className="perfromance-circles-div" style={{overflowY : (login_Data && login_Data.length >= 6) ? 'scroll' : 'auto'}}>
+                {class_Data.map((data,index)=>(
+                    <div style={{width : '100%',height : '270px',display : 'flex',flexDirection : 'column', alignItems : 'center', justifyContent:'center'}}>
+                    <div className="progress-circle" style={{position : 'relative',boxShadow : '0 0 10px rgba(0,0,0,0.5)',background : 'linear-gradient( #2cd4ee, #9654f2)',height : '150px', width : '150px'}}>
+                        <svg width="100" height="100" viewBox="0 0 100 100">
+                            <circle className="progress-bg" cx="50" cy="50" r="45" style={{stroke : '#edf0f6'}}></circle>
+                            <circle className={`progress-bar class-progress-bar-${index}`} style={{stroke : '#2cd8ef'}} cx="50" cy="50" r="45" stroke-dasharray="282.6" stroke-dashoffset="282.6"></circle>
+                        </svg>
+                        <span className={`header-span-circle-txt class-progress-bar-txt-${index}`} style={{color : '#fff',fontWeight : 'bold'}}>00%</span>
+                    </div>
+                    <h1 style={{color : '#543cc8'}}>{data.Class}</h1>
+                    </div>
+                ))}
+            </div>
+        </div>
     </div>
   )
 };
